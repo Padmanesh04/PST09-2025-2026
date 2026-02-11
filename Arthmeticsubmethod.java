259dd2bfc64a119aad975aecdb02a4fd57df9e5c
Padmanesh04 <padmaneshk@gmail.com>
2026-02-11 12:14:01 +0530

    Add files via upload

---
+++ Arthmeticsubmethod.java
@@ -0,0 +1,28 @@
+package pst;
+
+public class Arthmeticsubmethod {
+
+	public static void main(String[] args) {
+	System.out.println(add(10,20));
+
+	}
+static int add(int a,int b) {
+	System.out.println(subtract(20,10));
+	return a+b ;
+	
+}
+static int subtract(int a,int b) {
+	System.out.println(multiply(12,10));
+	return a-b;
+	
+}
+static int multiply(int a,int b) {
+	System.out.println(modules(10,2));
+	return a*b;
+	
+}
+static int modules(int a,int b) {
+	return a%b;
+	
+}
+}
