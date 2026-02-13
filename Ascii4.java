273c4b4954161c1a88d5d030fd9371265be3dcf9
Padmanesh04 <padmaneshk@gmail.com>
2026-02-13 21:34:31 +0530

    Add files via upload

---
+++ Ascii4.java
@@ -0,0 +1,13 @@
+package pst;
+
+public class Ascii4 {
+	public static void main(String[] args) {
+		 small();
+	}
+	 static void  small() {
+		 for (int i=97;i<=122;i++) {
+			 char num=(char) i;
+			 System.out.println(num); 
+		 }
+	 }
+}
