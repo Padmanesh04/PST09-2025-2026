0934aa03f05cb93d0548bab4cabb3a8a655615df
Padmanesh04 <padmaneshk@gmail.com>
2026-02-13 21:30:10 +0530

    Add files via upload

---
+++ Ascii3.java
@@ -0,0 +1,14 @@
+package pst;
+
+public class Ascii3 {
+
+	public static void main(String[] args) {
+	ascend();	
+	}
+ static void  ascend() {
+	 for (int i=65;i<=90;i++) {
+		 char num=(char) i;
+		 System.out.println(num); 
+	 }
+ }
+}
