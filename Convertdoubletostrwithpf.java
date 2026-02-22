b8c1683727eb4cf6dfd03d721435c08a8126d619
Padmanesh04 <padmaneshk@gmail.com>
2026-02-22 23:17:56 +0530

    Add files via upload

---
+++ Convertdoubletostrwithpf.java
@@ -0,0 +1,13 @@
+package pst;
+
+import java.util.Scanner;
+public class Convertdoubletostrwithpf {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter a number :");
+		 double n =sc.nextDouble();
+		 String sr=Double.toString(n);
+		 System.out.println(sr);
+	}
+
+}
