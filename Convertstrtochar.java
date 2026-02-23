4cdd685946268607e02fc272a61c42b14e876858
Padmanesh04 <padmaneshk@gmail.com>
2026-02-23 23:14:17 +0530

    Add files via upload

---
+++ Convertstrtochar.java
@@ -0,0 +1,13 @@
+package pst;
+
+import java.util.Scanner;
+public class Convertstrtochar {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter a string :");
+		 String s =sc.next(); 
+		 char cu=s.charAt(0);
+		 System.out.println(cu);
+	}
+
+}
