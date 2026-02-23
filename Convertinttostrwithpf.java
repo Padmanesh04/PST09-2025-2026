c744c71503eebaee47f0e7274d8fd462cb8215d6
Padmanesh04 <padmaneshk@gmail.com>
2026-02-23 23:06:32 +0530

    Add files via upload

---
+++ Convertinttostrwithpf.java
@@ -0,0 +1,14 @@
+package pst;
+
+import java.util.Scanner;
+
+public class Convertinttostrwithpf {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter a number :");
+		 int n =sc.nextInt();
+         String s=Integer.toString(n);
+         System.out.println(s);
+	}
+
+}
