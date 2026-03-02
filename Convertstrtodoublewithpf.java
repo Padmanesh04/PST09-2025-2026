607185bcfbc235f4c3085eb048c56ad70c3caec8
Padmanesh04 <padmaneshk@gmail.com>
2026-03-02 10:03:19 +0530

    Add files via upload

---
+++ Convertstrtodoublewithpf.java
@@ -0,0 +1,14 @@
+package pst;
+
+import java.util.Scanner;
+public class Convertstrtodoublewithpf {
+
+	public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	  System.out.println("enter a string:");
+	  String str=sc.next();
+	  Double dd=Double.parseDouble(str);
+      System.out.println(str);
+	}
+
+}
