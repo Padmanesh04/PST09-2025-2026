0cafee02dab6aad2e4a4c4465a44b3aa0561bf79
Padmanesh04 <padmaneshk@gmail.com>
2026-04-04 09:59:05 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,19 @@
+package pst;
+
+public class Name {
+
+	public static void main(String[] args) {
+		int a=12,b=23,c=22;
+	System.out.println(yogs(a,b));
+	System.out.println(yogs(a,b,c));
+
+	}
+	static int yogs(int a,int b) {
+		return 0;
+	}
+	static int yogs(int a,int b,int c) {
+		return c ;
+	}
+
+}
+
