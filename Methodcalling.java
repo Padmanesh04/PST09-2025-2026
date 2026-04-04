5df58bab8b6b425c1ac633e309150800fd1e65e1
Padmanesh04 <padmaneshk@gmail.com>
2026-04-04 09:54:15 +0530

    Add files via upload

---
+++ Methodcalling.java
@@ -0,0 +1,37 @@
+package pst;
+
+public class Methodcalling {
+
+	public static void main(String[] args) {
+		Aiml_1yr obj = new Aiml_1yr();
+		obj.jeyakumar();		}
+}
+ class Aiml_1yr{
+	 static void jeyakumar() {
+
+	   System.out.println("he is a 1st year");
+}
+	 static void ganesh() {
+	   System.out.println("he is also a 1st year");
+	 }
+	
+ }
+class Aiml_2yr{
+  static void akash() {
+	   System.out.println("he is  a 2nd year");
+  }
+  static void guru() {
+	  System.out.println("he is also a 2nd year");
+  }
+
+}
+class Aiml_3yr{
+	static void mahesh() {
+		 System.out.println("he is  a 3rd year");
+	  }
+	static void thangam() {
+		 System.out.println("he is also a 3rd year");
+	  }
+	}
+	
+
